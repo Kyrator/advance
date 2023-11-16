c74d0c9d99df1fb436a557a0fabaa5f63d76f405
Александр Сырысев <kyrator@bk.ru>
2023-11-16 17:15:28 +0300

    decided task2

--- module_02_linux/homework/hw2/get_mean_size.py
+++ module_02_linux/homework/hw2/get_mean_size.py
@@ -11,10 +11,19 @@ import sys
 
 
 def get_mean_size(ls_output: str) -> float:
-    ...
+    summ_file = 0
+    count = 0
+    for line in ls_output:
+        count += 1
+        summ_file += int(line.split()[4])
+    return summ_file / count
 
 
 if __name__ == '__main__':
-    data: str = sys.stdin.read()
-    mean_size: float = get_mean_size(data)
-    print(mean_size)
+    data: list = sys.stdin.readlines()
+    data = data[1:]
+    if not data:
+        print('Данная директория пуста или к ней нет доступа.')
+    else:
+        mean_size: float = get_mean_size(data)
+        print(f'Средний размер файлов в данной директории: {mean_size} байт')
