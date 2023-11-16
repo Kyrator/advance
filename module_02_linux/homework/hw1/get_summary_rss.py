defc3ec0a94b35e2dfecc1e279b2ccf5b78f9188
Александр Сырысев <kyrator@bk.ru>
2023-11-16 16:30:03 +0300

    decided task1

--- module_02_linux/homework/hw1/get_summary_rss.py
+++ module_02_linux/homework/hw1/get_summary_rss.py
@@ -12,13 +12,34 @@ $ ps aux > output_file.txt
 а возвращает суммарный объём потребляемой памяти в человекочитаемом формате.
 Это означает, что ответ надо перевести в байты, килобайты, мегабайты и так далее.
 """
+import os.path
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PATH_TO_OUTPUT_FILE = os.path.join(BASE_DIR, 'output_file.txt')
 
 
 def get_summary_rss(ps_output_file_path: str) -> str:
-    ...
+    """ func open file """
+    summa = 0
+    try:
+        with open(ps_output_file_path, 'r', encoding='UTF-8') as ps_output:
+            title_dict = ps_output.readline().split()
+            for ps_output_line in ps_output.readlines():
+                rss_data = ps_output_line.split()[5]
+                if rss_data.isdigit():
+                    summa += int(rss_data)
+    except FileNotFoundError:
+        print('File not found')
+
+    size = ['Б', 'Кб', 'Мб', 'Гб', 'Тб']
+    size_index = 0
+    while summa > 1024:
+        summa //= 1024
+        size_index += 1
+    return "Объем используемой памяти {summa} {size}".format(summa=summa, size=size[size_index])
 
 
 if __name__ == '__main__':
-    path: str = 'PATH_TO_OUTPUT_FILE'
+    path: str = PATH_TO_OUTPUT_FILE
     summary_rss: str = get_summary_rss(path)
     print(summary_rss)
