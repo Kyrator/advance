36671b6cfd152e17ae51f05a31073995f6730088
Александр Сырысев <kyrator@bk.ru>
2023-11-16 22:10:18 +0300

    decided task4

--- module_02_linux/homework/hw4/hello_word_with_name.py
+++ module_02_linux/homework/hw4/hello_word_with_name.py
@@ -8,13 +8,28 @@
 """
 
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
+weekdays_tuple = ('понедельника',
+                  'вторника',
+                  'среды',
+                  'четверга',
+                  'пятницы',
+                  'субботы',
+                  'воскресенья')
 
-@app.route('/hello-world/...')
-def hello_world():
-    ...
+
+@app.route('/hello-world/<user>')
+def hello_world(user: str):
+    weekday = datetime.today().weekday()
+    day = weekdays_tuple[weekday]
+    if weekday == 2 or weekday == 4 or weekday == 5:
+        nice = 'Хорошей'
+    else:
+        nice = 'Хорошего'
+    return f'Привет, {user}. {nice} {day}!<br>'
 
 
 if __name__ == '__main__':
