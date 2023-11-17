22eb12d8e133bddfb41f29b4b7e0a6808a34c832
Александр Сырысев <kyrator@bk.ru>
2023-11-17 12:47:51 +0300

    decided task6

--- module_02_linux/homework/hw6/head_file.py
+++ module_02_linux/homework/hw6/head_file.py
@@ -28,15 +28,26 @@ hello wo
 /home/user/module_2/docs/simple.txt 12
 hello world!
 """
+import os.path
 
 from flask import Flask
 
 app = Flask(__name__)
 
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 @app.route("/head_file/<int:size>/<path:relative_path>")
 def head_file(size: int, relative_path: str):
-    ...
+    abs_path = os.path.join(BASE_DIR, relative_path)
+    try:
+        with open(abs_path, 'r') as file:
+            resualt_text = file.read(size)
+            resualt_size = len(resualt_text)
+        return f'<b> {abs_path}</b> {resualt_size} <br> {resualt_text}'
+    except FileNotFoundError:
+        return f'<h1>Error. File does not exist!</h1>'
 
 
 if __name__ == "__main__":
