91c7497700b643f1e479604753fbeeb422e4a3c4
Александр Сырысев <kyrator@bk.ru>
2023-11-18 00:42:07 +0300

    decided task7

--- module_02_linux/homework/hw7/accounting.py
+++ module_02_linux/homework/hw7/accounting.py
@@ -11,6 +11,7 @@
 Дата для /add/ передаётся в формате YYYYMMDD, где YYYY — год, MM — месяц (от 1 до 12), DD — число (от 01 до 31).
 Гарантируется, что переданная дата имеет такой формат и она корректна (никаких 31 февраля).
 """
+import datetime
 
 from flask import Flask
 
@@ -21,18 +22,35 @@ storage = {}
 
 @app.route("/add/<date>/<int:number>")
 def add(date: str, number: int):
-    ...
+    expense: int = number
+    year, month, day = int(date[:4]), int(date[4:6]), int(date[6:8])
+    try:
+        _ = datetime.datetime(year, month, day)
+        storage.setdefault(year, {}).setdefault(month, 0)
+        storage[year][month] += expense
+        storage.setdefault(year, {}).setdefault("total", 0)
+        storage[year]['total'] += expense
+        return f'Запись произведена {storage}'
+    except ValueError:
+        return f'Такой даты не существует. Проверьте год - {year}, месяц - {month}, день - {day}'
 
 
 @app.route("/calculate/<int:year>")
 def calculate_year(year: int):
-    ...
+    try:
+        return f"{storage[year]['total']}"
+    except KeyError:
+        return f"{year} год пустой, заполните значения"
 
 
 @app.route("/calculate/<int:year>/<int:month>")
 def calculate_month(year: int, month: int):
-    ...
-
+    try:
+        return f"{storage[year][month]}"
+    except KeyError:
+        return f"{month} месяц {year} года пустой, заполните значения"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
