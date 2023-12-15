d99eafba872780b5e721ac7cd2c2f0b4e8c662fa
Александр Сырысев <kyrator@bk.ru>
2023-12-15 15:57:51 +0300

    decided task1

--- module_04_flask/homework/hw1_3/hw1_registration.py
+++ module_04_flask/homework/hw1_3/hw1_registration.py
@@ -8,21 +8,26 @@
 5) index (только числа, обязательно для заполнения);
 6) comment (текст, необязательно для заполнения).
 """
+import json
+from urllib.parse import unquote_plus
 
-from flask import Flask
+import requests
+from flask import Flask, request
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
+from wtforms.validators import Email, InputRequired, NumberRange
+
 from hw2_validators import number_length, NumberLength
 
 app = Flask(__name__)
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField()
-    phone = IntegerField()
-    name = StringField()
-    address = StringField()
-    index = IntegerField()
+    email = StringField(validators=[InputRequired(), Email()])
+    phone = IntegerField(validators=[InputRequired(), NumberRange(min=1_000_000_000, max=9_999_999_999)])
+    name = StringField(validators=[InputRequired()])
+    address = StringField(validators=[InputRequired()])
+    index = IntegerField(validators=[InputRequired()])
     comment = StringField()
 
 
@@ -31,11 +36,19 @@ def registration():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        email, phone = form.email.data, form.phone.data
+        email, name, phone, address = form.email.data, form.name.data, form.phone.data, form.address.data
 
         return f"Successfully registered user {email} with phone +7{phone}"
 
+    errors_list = form.errors
+    print(errors_list)
+
+    email_errors_list = form.email.errors
+    print(email_errors_list)
+
     return f"Invalid input, {form.errors}", 400
+
+
 
 
 if __name__ == "__main__":
