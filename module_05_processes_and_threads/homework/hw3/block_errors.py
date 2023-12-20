38835953ae7ebeb3a5942fd89e6f28029da25732
Александр Сырысев <kyrator@bk.ru>
2023-12-20 23:00:49 +0300

    decided task3

--- module_05_processes_and_threads/homework/hw3/block_errors.py
+++ module_05_processes_and_threads/homework/hw3/block_errors.py
@@ -2,22 +2,33 @@
 Реализуйте контекстный менеджер, который будет игнорировать переданные типы исключений, возникающие внутри блока with.
 Если выкидывается неожидаемый тип исключения, то он прокидывается выше.
 """
-
+import sys
 from typing import Collection, Type, Literal
 from types import TracebackType
 
 
 class BlockErrors:
     def __init__(self, errors: Collection) -> None:
-        ...
+        self.errors = errors
 
     def __enter__(self) -> None:
-        ...
+        self.stdout = sys.stderr
+        self.stderr = sys.stderr
+        # print("self.stdout", self.stdout)
+        # print("self.stderr", self.stderr)
 
     def __exit__(
             self,
             exc_type: Type[BaseException] | None,
             exc_val: BaseException | None,
             exc_tb: TracebackType | None
-    ) -> Literal[True] | None:
-        ...
+            ) -> Literal[True] | None:
+        # print("start end")
+        if issubclass(exc_type, tuple(self.errors)) or exc_type in self.errors:
+            return True
+        else:
+            print(exc_type.__name__, exc_val)
+            return None
+
+
+
--- module_05_processes_and_threads/homework/hw3/test_block_errors.py
+++ module_05_processes_and_threads/homework/hw3/test_block_errors.py
@@ -2,5 +2,54 @@ import unittest
 from block_errors import BlockErrors
 
 
+class TestBlockError(unittest.TestCase):
+    def test_err_types(self):
+        dict_err_types = {ZeroDivisionError, TypeError}
+        try:
+            with BlockErrors(dict_err_types):
+                a = 1 / 0
+        except:
+            self.assertRaises('Выполнено без ошибок')
+        # print(self)
+        # .assertTrue('Выполнено без ошибок' in self.)
+        """ Выполнено без ошибок """
+
+    def test_err_types2(self):
+        dict_err_types2 = {ZeroDivisionError}
+        try:
+            with BlockErrors(dict_err_types2):
+                a3 = 1 / '0'
+            print('Выполнено без ошибок')
+        except TypeError:
+            self.assertRaises(TypeError)
+
+        """TypeError: unsupported operand type(s) for /: 'int' and 'str'"""
+
+    def test_outer_err_types3(self):
+        dict_outer_err_types = {TypeError}
+        try:
+            with BlockErrors(dict_outer_err_types):
+                inner_err_types = {ZeroDivisionError}
+                with BlockErrors(inner_err_types):
+                    a1 = 1 / '0'
+                print('Внутренний блок: выполнено без ошибок')
+            print('Внешний блок: выполнено без ошибок')
+        except:
+            self.assertRaises('Внешний блок: выполнено без ошибок')
+
+        """Внешний блок: выполнено без ошибок"""
+
+    def test_err_types4(self):
+        dict_err_types = {Exception}
+        try:
+            with BlockErrors(dict_err_types):
+                a2 = 1 / '0'
+            print('Выполнено без ошибок')
+        except:
+            self.assertRaises('Выполнено без ошибок')
+
+        """Выполнено без ошибок"""
+
+
 if __name__ == '__main__':
     unittest.main()
