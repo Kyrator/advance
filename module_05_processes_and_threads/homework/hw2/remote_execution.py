110d679934e9e903e49c8f9adb685ee2f87d8aa1
Александр Сырысев <kyrator@bk.ru>
2023-12-20 19:02:25 +0300

    decided task2

--- module_05_processes_and_threads/homework/hw2/remote_execution.py
+++ module_05_processes_and_threads/homework/hw2/remote_execution.py
@@ -4,27 +4,47 @@
 Пользователю возвращается результат работы программы, а если время, отведённое на выполнение кода, истекло,
 то процесс завершается, после чего отправляется сообщение о том, что исполнение кода не уложилось в данное время.
 """
-
+import subprocess
+import shlex
 from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
+from wtforms.validators import InputRequired
 
 app = Flask(__name__)
 
 
 class CodeForm(FlaskForm):
-    code = StringField()
-    timeout = IntegerField()
+    code = StringField(validators=[InputRequired()])
+    timeout = IntegerField(default=10)
 
 
 def run_python_code_in_subproccess(code: str, timeout: int):
-    ...
+    cmd = f'prlimit --nproc=1:1 python -c "{code}"'
+    result = subprocess.Popen(
+        shlex.split(cmd),
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        )
+    was_killed_by_process = False
+    try:
+        out, err = result.communicate(timeout=timeout)
+    except subprocess.TimeoutExpired:
+        result.kill()
+        out, err = result.communicate()
+        was_killed_by_process = True
+    return out.decode(), err.decode(), was_killed_by_process
 
 
 @app.route('/run_code', methods=['POST'])
 def run_code():
-    ...
+    form = CodeForm()
+    if form.validate_on_submit():
+        out, err, kill = run_python_code_in_subproccess(form.code.data, form.timeout.data)
+        return f"Stdout: {out}, stderr: {err}, process was killed by timeout {kill}"
+    return f"Bad request. Error = {form.errors}", 400
 
 
 if __name__ == '__main__':
+    app.config["WTF_CSRF_ENABLED"] = False
     app.run(debug=True)
--- module_05_processes_and_threads/homework/hw2/test_remote_execution.py
+++ module_05_processes_and_threads/homework/hw2/test_remote_execution.py
@@ -2,5 +2,50 @@ import unittest
 from remote_execution import app
 
 
+class RemoteTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        app.config['WTF_CSRF_ENABLED'] = False
+        cls.app = app.test_client()
+        cls.base_url = '/run_code'
+
+    def test_timeout(self):
+        """Тайм-аут ниже, чем время исполнения"""
+        data = {
+            'code': 'print([i*i*i for i in range(20000000)])',
+            'timeout': 1,
+        }
+        response = self.app.post(self.base_url, data=data)
+        self.assertEqual(
+            response.data.decode(),
+            'Stdout: , stderr: , process was killed by timeout True'
+        )
+
+    def test_form_valid(self):
+        """Некорректно введённые данные в форме."""
+        data = {
+            'code': 'print("Hello World!")',
+            'timeout': 1,
+        }
+        response = self.app.post(self.base_url, data=data)
+        self.assertRaises(
+            TypeError
+        )
+
+    def test_shell_true(self):
+        """Безопасный ввод в поле с кодом (проверка на `shell=True`)"""
+        data = {
+            'code': 'print()"; echo "hacked',
+            'timeout': 3,
+        }
+        response = self.app.post(self.base_url, data=data)
+        self.assertRaises(
+            ValueError
+        )
+
+
 if __name__ == '__main__':
     unittest.main()
