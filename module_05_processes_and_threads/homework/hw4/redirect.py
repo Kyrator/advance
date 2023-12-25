d81854f687db020bea1f30aba7921c0b64dae964
Александр Сырысев <kyrator@bk.ru>
2023-12-25 21:20:27 +0300

    decided task4 only i didn't make test. I don't understand how make it ;(

--- module_05_processes_and_threads/homework/hw4/redirect.py
+++ module_05_processes_and_threads/homework/hw4/redirect.py
@@ -6,17 +6,25 @@
 Аргументы контекстного менеджера должны быть непозиционными,
 чтобы можно было ещё перенаправить только stdout или только stderr.
 """
-
+import sys
+import traceback
 from types import TracebackType
 from typing import Type, Literal, IO
 
 
 class Redirect:
     def __init__(self, stdout: IO = None, stderr: IO = None) -> None:
-        ...
+        """Перенаправляем поток вывода в файл"""
+        self.new_stdout = stdout if stdout else sys.stdout
+        self.new_stderr = stderr if stderr else sys.stderr
 
-    def __enter__(self):
-        ...
+    def __enter__(self) -> None:
+        """Сохраняем старый поток вывода"""
+        self.old_stdout = sys.stdout
+        self.old_stderr = sys.stderr
+
+        sys.stdout = self.new_stdout
+        sys.stderr = self.old_stderr
 
     def __exit__(
             self,
@@ -24,4 +32,30 @@ class Redirect:
             exc_val: BaseException | None,
             exc_tb: TracebackType | None
     ) -> Literal[True] | None:
-        ...
+        """Закрываем файл и возвращаем старый поток вывода"""
+        if exc_type is not None:
+            sys.stdout = self.old_stdout
+            sys.stderr = self.new_stderr.write(traceback.format_exc())
+            sys.stderr = self.old_stderr
+            self.new_stdout.close()
+            self.new_stderr.close()
+            return True
+        else:
+            sys.stdout = self.old_stdout
+            sys.stderr = self.old_stderr
+            self.old_stdout.close()
+            self.old_stderr.close()
+            return None
+
+
+if __name__ == '__main__':
+    print("Hello stdout")
+    stdout_file = open('stdout.txt', 'w')
+    stderr_file = open('stderr.txt', 'w')
+
+    with Redirect(stdout=stdout_file, stderr=stderr_file):
+        print("Hello stdout.txt")
+        raise Exception('Hello stderr.txt')
+
+    print('Hello stdout again')
+    raise Exception('Hello stderr')
--- module_05_processes_and_threads/homework/hw4/test_redirect.py
+++ module_05_processes_and_threads/homework/hw4/test_redirect.py
@@ -1,9 +1,42 @@
 import unittest
+from unittest import TestCase
 from redirect import Redirect
+
+
+class TestRedirect(TestCase):
+    def setUp(self):
+        self.redirect = Redirect
+        self.stdout_file = open('stdout.txt', 'w', encoding='UTF-8')
+        self.stderr_file = open('stderr.txt', 'w', encoding='UTF-8')
+
+    def test_redirect_stdout(self):
+        pass
+        # try:
+        #     with self.redirect(stdout=self.stdout_file):
+        #         print("Hello stdout.txt")
+        # except Exception:
+        #     pass
+        # try:
+        #     with open('stdout.txt', 'r') as file:
+        #         read_file = file.read()
+        # except Exception:
+        #     pass
+        #
+        # self.assertEqual('Hello stdout.txt', read_file)
+
+    def test_redirect_stderr(self):
+        pass
+        # with self.redirect(stderr=self.stderr_file):
+        #     print("Hello stderr.txt")
+        #     self.stderr_file.close()
+        # with open('stderr.txt', 'r') as file:
+        #     read_file = file.read()
+        #     file.close()
+        # self.assertEquals('Hello stdout.txt', read_file)
 
 
 if __name__ == '__main__':
     unittest.main()
-    # with open('test_results.txt', 'a') as test_file_stream:
-    #     runner = unittest.TextTestRunner(stream=test_file_stream)
-    #     unittest.main(testRunner=runner)
+    with open('test_results.txt', 'a') as test_file_stream:
+        runner = unittest.TextTestRunner(stream=test_file_stream)
+        unittest.main(testRunner=runner)
