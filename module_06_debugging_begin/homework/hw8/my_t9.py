05b750266a25cfed7a9f92873d697d0a80f2308c
Александр Сырысев <kyrator@bk.ru>
2024-01-19 16:47:03 +0300

    decided task8

--- module_06_debugging_begin/homework/hw8/my_t9.py
+++ module_06_debugging_begin/homework/hw8/my_t9.py
@@ -17,14 +17,39 @@
 Напишите функцию my_t9, которая принимает на вход строку, состоящую из цифр 2–9,
 и возвращает список слов английского языка, которые можно получить из этой последовательности цифр.
 """
+import json
+import re
 from typing import List
+
+digit_dict = {
+    2: '[a-cA-C]',
+    3: '[d-fD-F]',
+    4: '[g-iG-I]',
+    5: '[j-lJ-L]',
+    6: '[m-oM-O]',
+    7: '[p-sP-S]',
+    8: '[t-vT-V]',
+    9: '[w-zW-Z]',
+}
 
 
 def my_t9(input_numbers: str) -> List[str]:
-    ...
+    expression = []
+    for number in input_numbers:
+        expression += digit_dict.get(int(number))
+    pattern = ''.join(expression)
+    words_list = re.findall(r'\b{pattern}[ ]\b'.format(pattern=pattern),
+                            *data_into_list, re.M
+                            )
+    return words_list
 
 
 if __name__ == '__main__':
+    with open('words.txt', 'r') as file:
+        data = file.read()
+        data_into_list = data.replace('\n', ' ').split(",")
+
     numbers: str = input()
     words: List[str] = my_t9(numbers)
     print(*words, sep='\n')
+
