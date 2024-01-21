4496255f6b359e3ca1f28a609262dc827f1d3d9c
Александр Сырысев <kyrator@bk.ru>
2024-01-21 21:29:21 +0300

    decided task1

--- module_07_logging_part_2/homework/hw1_add_logging/app.py
+++ module_07_logging_part_2/homework/hw1_add_logging/app.py
@@ -1,9 +1,21 @@
 import sys
+import logging
 from utils import string_to_operator
 
 
+logger = logging.getLogger('app')
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - name=%(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
 def calc(args):
-    print("Arguments: ", args)
+    logger.info("Arguments: {args}".format(args=args))
+    # print("Arguments: ", args)
 
     num_1 = args[0]
     operator = args[1]
@@ -12,23 +24,31 @@ def calc(args):
     try:
         num_1 = float(num_1)
     except ValueError as e:
-        print("Error while converting number 1")
-        print(e)
+        # print("Error while converting number 1")
+        # print(e)
+        logger.exception("Error while converting number 1 {}".format(e))
+        raise ValueError(e)
 
     try:
         num_2 = float(num_2)
     except ValueError as e:
-        print("Error while converting number 1")
-        print(e)
+        # print("Error while converting number 1")
+        # print(e)
+        logger.exception("Error while converting number 2".format(e))
+        raise e
 
     operator_func = string_to_operator(operator)
-
     result = operator_func(num_1, num_2)
 
-    print("Result: ", result)
-    print(f"{num_1} {operator} {num_2} = {result}")
+    # print("Result: ", result)
+    logger.info("Result: {}".format(result))
+
+    # print(f"{num_1} {operator} {num_2} = {result}")
+    logger.info(f"{num_1} {operator} {num_2} = {result}")
 
 
 if __name__ == '__main__':
     # calc(sys.argv[1:])
     calc('2+3')
+
+
--- module_07_logging_part_2/homework/hw1_add_logging/utils.py
+++ module_07_logging_part_2/homework/hw1_add_logging/utils.py
@@ -1,7 +1,17 @@
-
+import logging
+import sys
 from typing import Union, Callable
 from operator import sub, mul, truediv, add
 
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - name=%(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 OPERATORS = {
     '+': add,
@@ -18,12 +28,20 @@ def string_to_operator(value: str) -> Callable[[Numeric, Numeric], Numeric]:
     Convert string to arithmetic function
     :param value: basic arithmetic function
     """
+    logger.info('Start def string_to_operator')
     if not isinstance(value, str):
-        print("wrong operator type", value)
-        raise ValueError("wrong operator type")
+        logger.exception("{e} \'{value}\'".format(e="wrong operator type", value=value))
+        raise "wrong operator type"
+        # logger.exception("{e} \'{value}\'".format(e="wrong operator type", value=value))
+        # raise ValueError
+        # print("wrong operator type", value)
+        # raise ValueError("wrong operator type")
 
     if value not in OPERATORS:
-        print("wrong operator value", value)
-        raise ValueError("wrong operator value")
-
+        logger.exception("{e} \'{value}\'".format(e="wrong operator value", value=value))
+        raise "wrong operator value"
+        # logger.exception("{e} \'{value}\'".format(e="wrong operator value", value=value))
+        # raise ValueError
+        # print("wrong operator value", value)
+        # raise ValueError("wrong operator value")
     return OPERATORS[value]
