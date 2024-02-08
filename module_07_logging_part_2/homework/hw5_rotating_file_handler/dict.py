9175a94938a93e67efa6a1a6ec31e6ca7fffcc77
Александр Сырысев <kyrator@bk.ru>
2024-02-08 20:52:31 +0300

    decided task6

---
+++ module_07_logging_part_2/homework/hw5_rotating_file_handler/dict.py
@@ -0,0 +1,101 @@
+import logging
+import logging.config
+import sys
+
+
+class LevelFileHandler(logging.Handler):
+    def __init__(self, file_name, mode='a'):
+        super().__init__()
+        self.file_name = file_name
+        self.mode = mode
+
+    def emit(self, record: logging.LogRecord) -> None:
+        message = self.format(record)
+        print(message)
+        if self.filter(record):
+            with open(self.file_name, mode=self.mode) as f:
+                f.write(message + '\n')
+
+
+class LevelFilter(logging.Filter):
+    def __init__(self, param):
+        super().__init__()
+        self.param = param
+
+    def filter(self, record):
+        return record.levelname == self.param
+
+
+dict_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "base": {
+            "format": "%(levelname)s | %(name)s | %(message)s"
+        }
+    },
+    "filters": {
+        'debug_filter': {
+            '()': LevelFilter,
+            "param": "DEBUG"
+        },
+        'error_filter': {
+            '()': LevelFilter,
+            "param": "ERROR"
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "base",
+            "stream": sys.stdout
+        },
+        "debug_file": {
+            "()": LevelFileHandler,
+            "level": "DEBUG",
+            "formatter": "base",
+            "file_name": "calc_debug.log",
+            "mode": "a",
+            "filters": ['debug_filter']
+        },
+        "error_file": {
+            "()": LevelFileHandler,
+            "level": "ERROR",
+            "formatter": "base",
+            "file_name": "calc_error.log",
+            "mode": "a",
+            "filters": ['error_filter']
+        },
+        "utils_file": {
+            "formatter": "base",
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "when": "H",
+            "interval": 10,
+            "backupCount": 1,
+            "filename": "utils.log"
+        },
+    },
+    "loggers": {
+        "app": {
+            "level": "DEBUG",
+            "handlers": ["debug_file", "error_file", "console"],
+            # "propagate": False,
+        },
+        "utils": {
+            "level": "INFO",
+            "handlers": ["utils_file", "console"],
+            # "propagate": False,
+        }
+    },
+
+    # "filters": {},
+    # "root": {} # == "": {}
+}
+
+
+def get_logger(name):
+    logging.config.dictConfig(dict_config)
+    logger = logging.getLogger(name)
+    return logger
+
---
+++ module_07_logging_part_2/homework/hw5_rotating_file_handler/utils.py
@@ -0,0 +1,40 @@
+import logging
+import sys
+from typing import Union, Callable
+from operator import sub, mul, truediv, add
+from dict import get_logger
+
+logger = get_logger("utils")
+
+OPERATORS = {
+    '+': add,
+    '-': sub,
+    '*': mul,
+    '/': truediv,
+}
+
+Numeric = Union[int, float]
+
+
+def string_to_operator(value: str) -> Callable[[Numeric, Numeric], Numeric]:
+    """
+    Convert string to arithmetic function
+    :param value: basic arithmetic function
+    """
+    logger.debug('Start def string_to_operator')
+    if not isinstance(value, str):
+        logger.exception("{e} \'{value}\'".format(e="wrong operator type", value=value))
+        raise "wrong operator type"
+        # logger.exception("{e} \'{value}\'".format(e="wrong operator type", value=value))
+        # raise ValueError
+        # print("wrong operator type", value)
+        # raise ValueError("wrong operator type")
+
+    if value not in OPERATORS:
+        logger.exception("{e} \'{value}\'".format(e="wrong operator value", value=value))
+        raise "wrong operator value"
+        # logger.exception("{e} \'{value}\'".format(e="wrong operator value", value=value))
+        # raise ValueError
+        # print("wrong operator value", value)
+        # raise ValueError("wrong operator value")
+    return OPERATORS[value]
